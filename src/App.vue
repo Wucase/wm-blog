798d6fbbd5126432089a31d8e2167fcc74266aa5
wuweiming <wuweiming@ideal.sh.cn>
2026-05-15 23:03:37 +0800

    feat: 测试

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <router-view v-slot="{ Component, route }">
     <transition name="app-route" mode="out-in">
+      22
       <component :is="Component" :key="route.fullPath" />
     </transition>
   </router-view>
